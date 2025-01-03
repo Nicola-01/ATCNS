8cdeccf0500b05212c4da2e7f5590ec7a9d22f3c
Nicola <nickbusato0101@gmail.com>
2025-01-03 15:31:16 +0100

    Update

--- Soot/src/main/java/org/example/Main.java
+++ Soot/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import soot.*;
+import soot.options.Options;
+
 public class Main {
     public static void main(String[] args) {
 
+        //prefer Android APK files// -src-prec apk
+        Options.v().set_src_prec(Options.src_prec_apk);
+
+        //output as APK, too//-f J
+        Options.v().set_output_format(Options.output_format_dex);
+
+        // resolve the PrintStream and System soot-classes
+        Scene.v().addBasicClass("java.io.PrintStream", SootClass.SIGNATURES);
+        Scene.v().addBasicClass("java.lang.System", SootClass.SIGNATURES);
     }
 }
